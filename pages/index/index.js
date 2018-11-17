30f78e7f47cab19c0607cf88803244fa3630978a
Tridu33 <2055969978@qq.com>
2018-11-17 19:32:22 +0800

    11-17

---
+++ pages/index/index.js
@@ -0,0 +1,39 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '吉大导览',
+    userInfo: {},
+    appInfo: {
+      logoUrl: '../../image/jlu.jpg',
+      title: '吉大导览'
+    },
+    jlu_places:{
+      name: "日新楼",
+      comment: "使人堕落的奢靡楼。",
+      imagePath: "/image/rixinloulouceng.jpg"
+    }
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../location/location'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    that.setData({
+      appInfo: this.data.appInfo
+    })
+    //调用应用实例的方法获取全局数据
+    // app.getUserInfo(function(userInfo){
+    //   //更新数据
+    //   that.setData({
+    //     userInfo:userInfo
+    //   })
+    //   that.update()
+    // })
+  }
+})
---
+++ resources/gis-school-spots.js
@@ -0,0 +1,14 @@
+module.exports=[
+    {
+        "id": 1,
+        "name": "前卫南区北门",
+        "longitude": "125.278176",
+        "latitude": "43.827741"
+    },
+    {
+        "id": 2,
+        "name": "地图说明",
+        "longitude": "125.053633",
+        "latitude": "44.733374"
+    }
+]
